5e7329259663966e11babb964456d05bb43723c6
hetalpat8 <hetalpat8@gmail.com>
2020-12-04 02:37:10 -0600

    travel project

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # regardless of operating system, where to find the confing file
 
 #we're telling the server, regardless of the file path, you can still find the information in this area:
-#windows = Documents\chicodes\chicodes_sept2020\week_5\in_class
-#Mac or Linux = Documents/chicodes_sept2020/week_5/in_class
+
 
 # Config Class
 # this will configure the database (when we have one) AND configure the
